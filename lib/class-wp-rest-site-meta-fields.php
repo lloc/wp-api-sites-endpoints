4c03d9440ba885718ba466d933cf385265275c82
Dennis Ploetner <re@lloc.de>
2026-08-14 10:16:56 +0200

    Class comment changed

--- lib/class-wp-rest-site-meta-fields.php
+++ lib/class-wp-rest-site-meta-fields.php
@@ -19,9 +19,8 @@ class WP_REST_Site_Meta_Fields extends WP_REST_Meta_Fields {
 	/**
 	 * Retrieves the object type for site meta.
 	 *
-	 * Site meta is stored in the blogmeta table and uses the `blog` meta type,
-	 * the same one `add_site_meta()` and `get_site_meta()` pass to the metadata
-	 * API. The `site` meta type resolves to sitemeta, which holds network meta.
+	 * Site meta lives in blogmeta. The `site` type would resolve to sitemeta,
+	 * which holds network meta.
 	 *
 	 * @return string The meta type.
 	 * @since x.x.x
